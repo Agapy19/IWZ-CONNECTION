3860d171ca5e2ce591a7bcbd74de7594d4bbd91e
Agapy Mafo <mafoagapy697@gmail.com>
2024-03-29 03:14:07 +0200

    condition sur le menu des notification

--- main.js
+++ main.js
@@ -11,6 +11,15 @@ sideBarMenu.forEach(item =>{
     item.addEventListener('click', () =>{
         changeActiveMenu();
         item.classList.add('active');
-        
+        if (item.id != 'notifications') {
+            document.querySelector('.container-notification').
+            style.display = 'none';
+        }
+        else{
+            document.querySelector('.container-notification').
+            style.display = 'block';
+            document.querySelector('#notifications .notification-count')
+            .style.display = 'none';
+        }
     })
 })
