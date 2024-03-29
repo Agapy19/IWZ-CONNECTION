407c5286b4bc64f2fa975b2dd1f916c5be8e91ef
Agapy Mafo <mafoagapy697@gmail.com>
2024-03-29 03:50:58 +0200

    ouvrire le container message lorsqu'on clique sur le menu message

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 // NAV
 const sideBarMenu = document.querySelectorAll('.side-bar__menu');
 
+// message
+const messages = document.querySelector('#message');
+const rigthMessages = document.querySelector('.rigth-messages');
+const message = messages.querySelectorAll('.messages-message');
+const messageSeach = document.querySelector('#message-seach');
+
 const changeActiveMenu = () =>{
     sideBarMenu.forEach(item =>{
         item.classList.remove('active');
@@ -22,4 +28,12 @@ sideBarMenu.forEach(item =>{
             .style.display = 'none';
         }
     })
+})
+
+message.addEventListener('click', () =>{
+    rigthMessages.style.boxShadow = '0 0 1rem var( --primary-color)';
+    messages.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none'
+    }, 2000);
 })
